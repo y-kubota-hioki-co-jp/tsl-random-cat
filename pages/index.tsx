239b9762bc7be420df81e668989e2103ad8f700a
Yu Kubota <y_kubota@hioki.co.jp>
2024-11-11 12:26:58 +0900

    [fix] 次の猫画像を表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     return (
         <div className={styles.page}>
             <button onClick={handleClick} className={styles.button}>
-                One more cat !
+                次の猫画像を表示
             </button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl} className={styles.img}/>}
